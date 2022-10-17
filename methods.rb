5cb43b7d902088a10d3d4a5286c5275a917440bb
Gloriousjoy <gloriousfab99@gmail.com>
2022-10-17 03:00:05 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+    puts "Hello programmer!"
+    end
+
+    #one argument
+    def greet(name)
+        puts "Hello, #{name}!"
+    end
+    #one argument with default parameter
+    def greet_with_default(name = "programmer")
+        puts "Hello, #{name}!"
+    end
+    # returns sum of two numbers
+    def add(num1, num2) 
+        num1 + num2;        
+    end
+    #halves
+    def halve(num)
+        return nil unless num.class == Integer
+        num / 2
+    end
